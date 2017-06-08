af46a078e6affcc6374443bc7e17e446508f4640
tonyzhao <tonyzhao1650@gmail.com>
2017-06-08 11:41:23 +0800

    re-write container example

--- relay/relay-official-guide.js
+++ relay/relay-official-guide.js
@@ -1,81 +1,81 @@
 import React from 'react'
-
 class ProfilePicture extends React.Component {
-	render() {
-		var user = this.props.user
-		var variables = this.props.relay.variables;
-		return (
-			<View>
-				<Image uri={user.profilePhoto.uri} width={variables.size} />
-				<Slider onChange={value => this.setSize(value)} />
-			</View>
-		)
-	}
+    render() {
+        var user = this.props.user
+        var variables = this.props.relay.variables
+        return (
+            <View>
+                <Image uri={user.profilePhoto.uri} width={variables.size} />
+                <Slider onChange={value => this.setSize(value)} />
+            </View>
+        )
+    }
 
-	setSize(photoSize) {
-		// TODO: Fetch the profile photo uri for the given size
-		this.props.relay.setVariables({
-			size: photoSize,
-		})
-	}
+    setSize(photoSize) {
+        // TODO: Fetch the profile photo uri for the given size...
+        this.props.relay.setVariables({
+            size: photoSize,
+        })
+    }
 }
 
 export default Relay.createContainer(ProfilePicture, {
-	initialVariables: {
-		size: 32
-	},
+    initialVariables: {
+        size: 32
+    },
 
-	fragments: {
-		user: () => Relay.QL`
-			fragment on User {
-				profilePhoto(size: $size) {
-					uri,
-				},
-			}
-		`,
-	}
-}
+    fragments: {
+        user: () => Relay.QL`
+            fragment on User {
+                profilePhoto(size: $size) {
+                    uri,
+                },
+            }
+        `,
+    },
+})
+
+
+// composition
+// compose the view logic
+// compose the data descriptions
 
 class Profile extends React.Component {
-	render() {
-		var user = this.props.user
-		return (
-			<View>
-				<ProfilePicture user={user} />
-				<Text>{user.name}</Text>
-			</View>
-		)
-	}
+    render() {
+        var user = this.props.user
+        return (
+           <View>
+               <ProfilePicture user={user} />
+               <Text>{user.name}</Text>
+           </View>
+        )
+    }
 }
 
-// Composing Fragments
+
 export default Relay.createContainer(Profile, {
-	fragments: {
-		user: () => Relay.QL`
-			fragment on User {
-				name,
-				${ProfilePicture.getFragment('user')},
-			}
-		`,
-	}
+    fragments: {
+        user: () => Relay.QL`
+            feagment on User {
+                name,
+                ${ProfilePicture.getFragment('user')},
+            }
+        `,
+    }
 })
 
 // is equivalent to the following
 //`
-//	fragment Profile on User {
-//		name,
-//		...ProfilePhoto,
-//	}
+//    fragment Profile on User {
+//        name,
+//        ...ProfilePhoto,
+//    }
 //
-//	fragment ProfilePhoto on User {
-//		profilePhoto(size: $size) {
-//			uri,
-//		},
-//	}
+//    fragment ProfilePhoto on User {
+//        profilePhoto(size: $size) {
+//            uri,
+//        },
+//    }
 //`
 
 
-
-
-
-	
