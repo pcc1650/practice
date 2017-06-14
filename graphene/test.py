fc0cbecb97c73cbfa65d92b44e42e1e2c4e54d43
tonyzhao <tonyzhao1650@gmail.com>
2017-06-14 10:25:20 +0800

    finish tutorial

--- graphene/test.py
+++ graphene/test.py
@@ -1,3 +1,4 @@
+# 1. Getting Started
 # A GraphQL schema describes your data model, and provides a GraphQL server with associated set of resolve methods that know how to fetch data.
 import graphene
 
@@ -9,7 +10,7 @@ class Query(graphene.ObjectType):
         
 schema = graphene.Schema(query=Query)
 
-
+# 2. Types Reference
 # Graphene defines the following base Scalar Types: 
 # graphene.String, graphene.Int, graphene.Float, graphene.Boolean, graphene.ID
 # custom scalars: graphene.types.datetime.DateTime, graphene.types.datetime.Time, graphene.types.json.JSONString
@@ -151,3 +152,47 @@ class CreatePerson(graphene.Mutation):
 # InputFields are used in mutations to allow nested input data for mutations
 # To use an InputField you define an InputObjectType that specifies the structure of your input data.
 # InputObjectTypes can also be fields of InputObjectTypes allowing you to have as complex of input data as you need.
+
+
+
+# 3. Execution
+# schema = graphene.Schema(...)
+# result = schema.execute('{ name }')
+# result represents the result of execution. result.data is the result of executing the query, result.errors is None if no errors occured, and is a non-empty list if an error occurred.
+
+
+# Middleware
+# Dataloader
+
+
+
+# 4. Relay
+# Nodes
+# A node is an Interface provided by graphene.relay that contains a single firld id(which is a ID!). Any object that inherits from it has to implement a get_node method for retrieving a Node by an id.
+class Ship(graphene.ObjectType):
+    class Meta: 
+        interfaces = (relay.Node, )
+
+    name = graphene.String(description='The name of the ship')
+
+    @classmethod
+    def get_node(cls, id, context, info):
+        return get_ship(id)
+# The id returned by the Ship type when you query it will be a scalar which contains enough info for the server to know its type and its id.
+# Custom Nodes ...
+# Accessing node types
+# If we want to retrieve node instances from a global_id(scalar that identifies an instance by it's type name and id), we can simple do Node.get_node_from_global_id(global_id, context, info)
+# In the case we want to restrict the instance retrieval to a specific type, we can do:
+# Node.get_node_from_global_id(global_id, context, info, only_type=Ship). This will raise an error if the global_id doesn't correspond to a Ship type.
+
+# Node Root field
+# As is required in the Relay specification, the server must implement a root field called node that returns a Node Interface.
+# For this reason, graphene provides the field relay.Node.Field, which links to any type in the Schema which implements Node.
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+
+# Connection
+# A connection is a vitaminized version of a List that provides ways of slicing and paginating through it.
+# ????????
+
+
